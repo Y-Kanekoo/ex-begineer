5b297f06b41295647439eab18e7a3c63efe55d02
Y-Kanekoo <kaneko.yuki2001@gmail.con>
2025-05-19 17:56:21 +0900

    exam04　途中

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -1,4 +1,25 @@
 package com.example.controller;
 
+
+import com.example.form.UserForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam04")
 public class Exam04Controller {
+
+    @GetMapping("")
+    public String index(UserForm userForm){
+        return "exam04";
+    }
+
+    @PostMapping("")
+    public String registerUser(){
+
+        return "exam04-result";
+    }
 }
