cf268c69a6e09682daa7be7e9598017bc3586958
Y-Kanekoo <kaneko.yuki2001@gmail.con>
2025-05-19 16:40:22 +0900

    変数修正した

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -20,9 +20,9 @@ public class Exam03Controller {
 
     @RequestMapping("/purchase")
     public String purchase(Integer item1, Integer item2, Integer item3){
-        int total = item1 + item2 + item3;
-        int totalwithtax = (int) (total * 1.1);
-        application.setAttribute("total",total);
+        int subtotal = item1 + item2 + item3;
+        int totalwithtax = (int) (subtotal * 1.1);
+        application.setAttribute("subtotal",subtotal);
         application.setAttribute("totalwithtax", totalwithtax);
         return "exam03-result";
     }
